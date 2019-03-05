9ef94d4daff01074ab7493cdf1dabbdc2a5c00a2
Praneeth <rupavataram-ram.praneeth@capgemini.com>
2019-03-05 10:51:36 +0530

    divison function updated

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,10 @@ public class Calculator{
 	public static void addition(int number1,int number2){
 		System.out.print("Addition=:"+(number1+number2));
 		}
+	public static void division(int number1,int number2){
+		if(number2==0)
+			System.out.print("division by zero");
+		else
+		System.out.print("division=:"+(number1/number2));
+		}
 }
