ebf2751a26ff8b809bd39ef9fee2ccf067692b80
Praneeth <rupavataram-ram.praneeth@capgemini.com>
2019-03-05 10:42:25 +0530

    subtraction function updated

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,7 @@ public class Calculator{
 	public static void addition(int number1,int number2){
 		System.out.print("Addition=:"+(number1+number2));
 		}
+	public static void subtraction(int number1,int number2){
+		System.out.print("subtraction=:"+(number1-number2));
+		}
 }
