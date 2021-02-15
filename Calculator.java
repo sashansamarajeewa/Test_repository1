585dfc9b17f051acfa1a30ffd91befaa105080f9
sashansamarajeewa <sashan@wso2.com>
2021-02-15 11:02:36 +0530

    Code formatting

--- Calculator.java
+++ Calculator.java
@@ -1,28 +1,30 @@
-public class Calculator{
-	public static void main(String a[]){
-		System.out.println("Hello world");
-		System.out.println("Calculator java program");
+public class Calculator {
+    public static void main(String a[]) {
+        System.out.println("Hello world");
+        System.out.println("Calculator java program");
 
-		}
-	public static void addition(int number1,int number2){
-		System.out.print("Addition=:"+(number1+number2));
-		}
+    }
 
-	public static void division(int number1,int number2){
-		if(number2==0)
-			System.out.print("division by zero");
-		else
-		System.out.print("division=:"+(number1/number2));
+    public static void addition(int number1, int number2) {
+        System.out.print("Addition=:" + (number1 + number2));
+    }
 
-	public static void subtraction(int number1,int number2){
-		System.out.print("subtraction=:"+(number1-number2));
-		}
-	public static void multiplication(int number1,int number2){
-		System.out.print("multiplication=:"+(number1*number2));
+    public static void division(int number1, int number2) {
+        if (number2 == 0)
+            System.out.print("division by zero");
+        else
+            System.out.print("division=:" + (number1 / number2));
+    }
 
-		}
-	public static void square(int number1){
-		System.out.print("square=:"+(number1*number1));
+    public static void subtraction(int number1, int number2) {
+        System.out.print("subtraction=:" + (number1 - number2));
+    }
 
-		}
-}
+    public static void multiplication(int number1, int number2) {
+        System.out.print("multiplication=:" + (number1 * number2));
+    }
+
+    public static void square(int number1) {
+        System.out.print("square=:" + (number1 * number1));
+    }
+
